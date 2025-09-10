a552948e5fd652548435368324be15b2b5a2cc3e
Logan <loroscom@purdue.edu>
2025-09-10 23:21:30 +0000

    Project 1 C code

--- Comp_Network_projects/client_files/client.c
+++ Comp_Network_projects/client_files/client.c
@@ -1,3 +1,5 @@
+//C code for client
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- Comp_Network_projects/server_files/server.c
+++ Comp_Network_projects/server_files/server.c
@@ -1,3 +1,5 @@
+//C code for server
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
